70f57f8f639684a9d70b7ba8d32d21eb9d6d3d4d
Vikash Gupta <gupta.vikash@mayo.edu>
2024-02-24 05:32:30 -0500

    Updated trt model in prepare demo

--- prepare_demo.sh
+++ prepare_demo.sh
@@ -1,13 +1,13 @@
 # Install gdown 
-pip install --upgrade --no-cache-dir gdown
+#pip install --upgrade --no-cache-dir gdown
 
 # Download large files and example data 
-gdown --folder https://drive.google.com/drive/folders/1Y863tgKMdmB_E_Y0fF5MrH5qdK5Hqp0B?usp=sharing
+#gdown --folder https://drive.google.com/drive/folders/1Y863tgKMdmB_E_Y0fF5MrH5qdK5Hqp0B?usp=sharing
 
 # Copy files to appropriate folders 
 mkdir -p TritonInferenceServer/data
-cp MONAI-model-inference/data_store/breast_density/breast.jpg TritonInferenceServer/data/breast.jpg
-cp MONAI-model-inference/data_store/breast_density/test.nii.gz TritonInferenceServer/data/test.nii.gz
-
+cp -v MONAI-model-inference/data_store/breast_density/breast.jpg TritonInferenceServer/data/breast.jpg
+cp -v MONAI-model-inference/data_store/lv_segmentation/test.nii.gz TritonInferenceServer/data/test.nii.gz
+cp -rv MONAI-model-inference/model_repository TritonInferenceServer
  
 
